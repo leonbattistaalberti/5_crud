e9de1d9a37f30f0e90d6ae25d607409cbc84569c
thelonehegelian <thelonehegelian@gmail.com>
2020-08-21 10:26:38 +0100

    Use .env file instead of .secrets, as .secretes method did not work

--- frontend/truffle-config.js
+++ frontend/truffle-config.js
@@ -1,17 +1,14 @@
-const fs = require("fs");
 const HDWalletProvider = require("truffle-hdwallet-provider");
 
-const secrets = JSON.parse(fs.readFileSync(".secrets").toString.trim());
-
 module.exports = {
-	network: {
+	networks: {
 		ropsten: {
 			provider: () =>
 				new HDWalletProvider(
-					secrets.seed,
-					`https://ropsten.infura.io/v3/${secrets.projecId}`
+					process.env.SEED,
+					`https://ropsten.infura.io/v3/${process.env.PROJECT_ID}`
 				),
-			network_id: 3,
+			network_id: "3",
 		},
 	},
 };
